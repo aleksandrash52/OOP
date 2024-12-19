51b46b343214bae72124e6aa9bfa668072bf9856
aleksandrash52 <98161946+aleksandrash52@users.noreply.github.com>
2024-12-19 22:00:21 +0300

    Create NPC.h

---
+++ lab6/src/NPC.h
@@ -0,0 +1,32 @@
+#ifndef NPC_H
+#define NPC_H
+
+#include <string>
+#include <iostream>
+
+class NPC {
+public:
+    virtual ~NPC() = default;
+
+    virtual std::string getType() const = 0;
+    virtual void print(std::ostream& os) const = 0;
+    virtual void read(std::istream& is) = 0;
+    virtual bool canAttack(const NPC& other) const = 0;
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+    const std::string& getName() const { return name; }
+
+    void setX(int x) { this->x = x; }
+    void setY(int y) { this->y = y; }
+    void setName(const std::string& name) { this->name = name; }
+
+protected:
+    int x, y;
+    std::string name;
+};
+
+std::ostream& operator<<(std::ostream& os, const NPC& npc);
+std::istream& operator>>(std::istream& is, NPC& npc);
+
+#endif 
